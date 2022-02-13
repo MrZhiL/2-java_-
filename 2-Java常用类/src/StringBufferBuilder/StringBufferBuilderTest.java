9caa7c18acc586d78f50f09c9c8041df3775cf0f
Mrzhi <zhilx1997@sina.com>
2022-02-13 16:16:24 +0800

    v2.9 添加了String\StringBuffer\StringBuilder的效率测试

--- 2-Java常用类/src/StringBufferBuilder/StringBufferBuilderTest.java
+++ 2-Java常用类/src/StringBufferBuilder/StringBufferBuilderTest.java
@@ -12,6 +12,8 @@ import org.junit.Test;
  *      - String: 不可变的字符序列，底层使用char[]存储
  *      - StringBuffer:可变的字符序列；线程安全的，效率低；底层使用char[]存储
  *      - StringBuilder: 可变的字符序列；jdk5.0新增的，为线程不安全的，效率高；底层使用char[]存储
+ *
+ *      - 效率 ：StringBuilder > StringBuffer > String
  */
 public class StringBufferBuilderTest {
     @Test
@@ -29,5 +31,42 @@ public class StringBufferBuilderTest {
         System.out.println(strBuff1.length()); // 8
         System.out.println(strBuff1.capacity()); // 19
 
+    }
+
+    @Test
+    public void test02() {
+        // String\StringBuffer\StringBuilder三者效率的对比
+        long startTime = 0L;
+        long endTime = 0L;
+
+        String test = "";
+        StringBuffer buffer = new StringBuffer();
+        StringBuilder builder = new StringBuilder();
+
+        // 开始对比:StringBuffer
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < 20000; ++i) {
+            // public static String valueOf(int i) : 返回int参数的字符串表示形式。
+            buffer.append(String.valueOf(i));
+        }
+        endTime = System.currentTimeMillis();
+        System.out.println("Buffer runtime:" + (endTime - startTime));
+
+        // 开始对比:StringBuilder
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < 20000; ++i) {
+            // public static String valueOf(int i) : 返回int参数的字符串表示形式。
+            builder.append(String.valueOf(i));
+        }
+        endTime = System.currentTimeMillis();
+        System.out.println("Builder runtime:" + (endTime - startTime));
+
+        // 开始对比:String
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < 20000; ++i) {
+            test += i;
+        }
+        endTime = System.currentTimeMillis();
+        System.out.println("String runtime:" + (endTime - startTime));
     }
 }
