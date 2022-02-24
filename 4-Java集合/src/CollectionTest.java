7a42572c1547148b0d5803cdabee477be29eb7d9
Mrzhi <zhilx1997@sina.com>
2022-02-24 12:53:19 +0800

    v4.0 initial Java Collection and Map, add Collection Structure Test

---
+++ 4-Java集合/src/CollectionTest.java
@@ -0,0 +1,32 @@
+package src;
+
+/**
+ * @ClassName: CollectionTest.java
+ * @Description: Java - 集合框架的概述
+ * @author: zhilx (zhilx1997@sina.com)
+ * @version: v1.0
+ * @data: 2022/2/24 11:05
+ * @node: 集合框架的概述
+ *          1. 集合、数组都是对多个数据进行存储操作的结构，简称Java容器
+ *             说明：此时的存储，主要指的是内存层面的存储，不涉及到持久化的存储（.txt, .avi, 数据库中的存储等）
+ *          2.1 数组在春初多个数据方面的特点：
+ *              - 一旦初始化以后，其长度就固定了
+ *              - 数组一旦定义好，其元素的类型也就确定了。我们也就只能操作指定类型的数据了，
+ *                比如：String[] arr, int[] arr1, Object[] arr2
+ *          2.2 数组在存储多个数据方面的缺点：
+ *              - 一旦初始化以后，其长度就不可修改
+ *              - 数组中提供的方法非常有限，对于添加、删除、插入数据等操作，非常不便
+ *              - 获取数组中实际元素的灌输的需求，数组没有线程的属性或方法可用
+ *              - 数组存储数据的特点：有序、可重复。对于无序、不可重复的需求，不能满足
+ *
+ *          3. Java集合可分为Collection和Map两种体系
+ *              - Collection接口：单列数据，定义了存取一组对象的方法的集合
+ *                  - List：元素有序、可重复的集合  --> "动态"数组
+ *                    -- ArrayList、LinkedList、Vector
+ *                  - Set：元素无序、不可重复的集合  --> "集合"
+ *                    -- HashSet、LinkedHashset、TreeSet
+*               - Map接口：双列数据，用来存储一对（key - value）的数据。
+ *                    -- HashMap、LinkedHashMap、TreeMap、Hashtable、Properties
+ */
+public class CollectionTest {
+}
