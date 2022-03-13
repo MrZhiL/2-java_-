d3b9fd6e88d296a3913cbe1647a2eb8eb0dfa122
Mrzhi <zhilx1997@sina.com>
2022-03-13 17:05:39 +0800

    add 5-泛型/src/test2/Person.java, Student，添加了关于有限制条件的通配符的使用

---
+++ 5-泛型/src/test2/Person.java
@@ -0,0 +1,12 @@
+package src.test2;
+
+/**
+ * @ClassName: src.test2
+ * @Description: Java
+ * @author: zhilx (zhilx1997@sina.com)
+ * @version: v1.0
+ * @data: 2022/3/6 17:02
+ * @node:
+ */
+public class Person {
+}
