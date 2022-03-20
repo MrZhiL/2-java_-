3ed111f8fea18c58b83815ae2caf7276bf3bd9af
Mrzhi <zhilx1997@sina.com>
2022-03-20 22:31:11 +0800

    v7.6 添加UDP的连续输入测试代码

--- 7-网络编程/src/UDPTest.java
+++ 7-网络编程/src/UDPTest.java
@@ -5,6 +5,7 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 /**
  * @ClassName: UDPTest
@@ -19,9 +20,13 @@ import java.nio.charset.StandardCharsets;
  *          - UDP协议中每个数据报都给出了完成的地址信息，因此无序建立发送方和接收方的连接。
  */
 public class UDPTest {
-    @Test
+    public static void main(String[] args) {
+        UDPTest.sender();
+    }
+
+//    @Test
     // UDP的发送端
-    public void sender() {
+    public static void sender() {
         DatagramSocket datagramSocket = null; // 此时可以不用指定IP地址和端口号
         try {
             // 1. 使用类DatagramSocket 建立UDP的套接字
@@ -31,12 +36,25 @@ public class UDPTest {
             // 如果 datagramSocket 没有指定IP和端口号，则需要在这里进行绑定
             String str = new String("Hello, 这是使用UDP发送的数据报！");
             byte[] data = str.getBytes(StandardCharsets.UTF_8);
-
             InetAddress inetAddress = InetAddress.getByName("127.0.0.1");
-            DatagramPacket datagramPacket = new DatagramPacket(data, 0, data.length, inetAddress, 8081);
+            DatagramPacket datagramPacket = null;
 
-            // 3. 调用DatagramSocket的send()方法发送数据
+            datagramPacket = new DatagramPacket(data, 0, data.length, inetAddress, 8081);
             datagramSocket.send(datagramPacket);
+
+            // 连续发送数据
+            while (true) {
+                str = new String(new Scanner(System.in).nextLine());
+                data = str.getBytes(StandardCharsets.UTF_8);
+                datagramPacket = new DatagramPacket(data, 0, data.length, inetAddress, 8081);
+
+                System.out.println(str);
+                // 3. 调用DatagramSocket的send()方法发送数据
+                datagramSocket.send(datagramPacket);
+                if ("exit".equalsIgnoreCase(str) || "e".equalsIgnoreCase(str)) {
+                    break;
+                }
+            }
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
@@ -56,14 +74,24 @@ public class UDPTest {
             socket = new DatagramSocket(8081);
 
             // 2. 建立数据报
-            byte[] buffer = new byte[100]; // 因此使用UDP接收数据报时，需要一次性全部接收，如果接收不完则会抛弃，因此可以将该数组设置大一点
-            DatagramPacket packet = new DatagramPacket(buffer, 0, buffer.length);
+            byte[] buffer = null; // 因此使用UDP接收数据报时，需要一次性全部接收，如果接收不完则会抛弃，因此可以将该数组设置大一点
+            DatagramPacket packet = null;
 
-            // 3. 调用DatagramSocket的receive()方法进行数据接收，并存放如packet中
-            socket.receive(packet);
+            // 连续接收数据，直到收到exit或e后退出
+            while (true) {
+                buffer = new byte[100];
+                packet = new DatagramPacket(buffer, 0, buffer.length);
 
-            // 4. 将数据进行打印
-            System.out.println(new String(packet.getData(), 0, packet.getLength()));
+                // 3. 调用DatagramSocket的receive()方法进行数据接收，并存放如packet中
+                socket.receive(packet);
+
+                // 4. 将数据进行打印
+                String str = new String(new String(packet.getData(), 0, packet.getLength()));
+                if ("exit".equalsIgnoreCase(str) || "e".equalsIgnoreCase(str)) {
+                    break;
+                }
+                System.out.println(str);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
